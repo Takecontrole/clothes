ab98301b307380754866f3e3ca6f513ed8620bd3
Takecontrole <takecontrole2034@gmail.com>
2023-01-01 17:48:40 +0400

    removed photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 export const productImages = [
-    require('./images/6YEFp1_MXe4.jpg'),
+    
     require('./images/dei-r-0JFNDQKSknA-unsplash.jpg'),
     require('./images/ian-dooley-iGh7qbW9kUM-unsplash.jpg'),
     require('./images/raphael-nast-HyaSbSRA39A-unsplash.jpg'),
